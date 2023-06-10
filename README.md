e3a63e1427e543ef87040e4dcc812ae2cd178fec
Eduardo0409 <100179167+Eduardo0409@users.noreply.github.com>
2023-06-10 20:31:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contador-de-juros
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-gj4cdf)
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: `./main.html`,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
